0193ae3e00b8e03c38bd7c7eb06e7ff24ff60cd2
nazgab <gabdolnazym@gmail.com>
2025-12-06 03:47:50 +0500

    Update settings.py

--- bloodsite/settings.py
+++ bloodsite/settings.py
@@ -10,13 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 DEBUG = os.getenv("DEBUG", "true").lower() == "true"
 
-ALLOWED_HOSTS = [
-    h.strip()
-    for h in os.getenv(
-        "ALLOWED_HOSTS", "*"
-    ).split(",")
-    if h.strip()
-]
+ALLOWED_HOSTS = ["*", ".onrender.com", "localhost", "127.0.0.1"]
+
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
